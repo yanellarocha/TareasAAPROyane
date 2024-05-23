3553c4ee5ea8c7f45c336572b67f80bde4f98e1c
yanellarocha <166852280+yanellarocha@users.noreply.github.com>
2024-05-23 11:26:04 -0300

    tarea numero 11 js

---
+++ tareanumero11.js
@@ -0,0 +1,43 @@
+var precioFinal = 10000;
+var topping = prompt("Ingrese su sabor de topping");
+if(topping == "oreo"){
+    precioFinal = precioFinal + 10
+} else if(topping == 'kitkat'){
+    precioFinal = precioFinal + 15
+} else if(topping == 'kinder'){
+    precioFinal = precioFinal + 25
+} else {
+    console.log("No tenemos ese topping");
+}
+
+console.log("El precio del helado es de " + precioFinal)
+
+switch(pedido){
+    case "carne":
+        console.log("vino tinto")
+        break;
+    case "pescado":
+        break
+    case "verdura":
+        console.log("agua")
+        break
+    default:
+        break;
+}
+
+var arrayDeFrutas = [];
+var arrayDeFrutas = [];
+for(let index = 0; index < arrayDeFrutas.length; index++){
+    arrayDeFrutas.push(arrayDeFrutas[index]);
+}
+
+for(let index = 0; index <array.length; index++) {
+    console.log(arrayMostrar[index]);
+}
+
+var numero = 0;
+
+while(numero<11){
+    console.log(numero + "es mayor que 11")
+    numero++
+}
